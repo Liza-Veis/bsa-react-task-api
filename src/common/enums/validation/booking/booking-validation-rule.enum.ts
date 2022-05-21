0fb9da1a281c2698581ecaaba4eb0fc59fb15b56
Yelyzaveta Veis <liza.veis14@gmail.com>
2022-05-21 15:36:16 +0200

    development: * validation messages

--- src/common/enums/validation/booking/booking-validation-rule.enum.ts
+++ src/common/enums/validation/booking/booking-validation-rule.enum.ts
@@ -1,5 +1,6 @@
 const BookingValidationRule = {
   GUESTS_MIN_COUNT: 1,
+  GUESTS_MAX_COUNT: 10,
   DATE_GREATER_THEN: 'now',
 } as const;
 
--- src/validation-schemas/booking/create-booking/create-booking.validation-schema.ts
+++ src/validation-schemas/booking/create-booking/create-booking.validation-schema.ts
@@ -1,5 +1,8 @@
 import * as Joi from 'joi';
-import { BookingValidationMessage, BookingValidationRule } from '~/common/enums/enums';
+import {
+  BookingValidationMessage,
+  BookingValidationRule,
+} from '~/common/enums/enums';
 import { CreateBookingDto } from '~/common/types/types';
 
 const createBooking = Joi.object<CreateBookingDto>({
@@ -9,10 +12,15 @@ const createBooking = Joi.object<CreateBookingDto>({
   userId: Joi.string().trim().required().messages({
     'string.empty': BookingValidationMessage.USER_ID_REQUIRED,
   }),
-  guests: Joi.number().min(BookingValidationRule.GUESTS_MIN_COUNT).required().messages({
-    'number.empty': BookingValidationMessage.GUESTS_COUNT_REQUIRED,
-    'number.min': BookingValidationMessage.GUESTS_MIN_COUNT,
-  }),
+  guests: Joi.number()
+    .min(BookingValidationRule.GUESTS_MIN_COUNT)
+    .max(BookingValidationRule.GUESTS_MAX_COUNT)
+    .required()
+    .messages({
+      'number.empty': BookingValidationMessage.GUESTS_COUNT_REQUIRED,
+      'number.min': BookingValidationMessage.GUESTS_MIN_COUNT,
+      'number.max': BookingValidationMessage.GUESTS_MAX_COUNT,
+    }),
   date: Joi.date().greater('now').required().messages({
     'date.empty': BookingValidationMessage.DATE_REQUIRED,
     'date.base': BookingValidationMessage.DATE_INVALID,
